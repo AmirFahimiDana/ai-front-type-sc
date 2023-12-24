abea5000983c3bd2a994da373649c5837b67ea4c
amirfahimidana <amir.fahimi.aviation@gmail.com>
2023-12-25 00:45:27 +0330

    add local storage for save question example value

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { examples } from '../../data/example'
 import { IReasearchGate } from '../../interface/iResearchGate'
 import { Link } from 'react-router-dom';
@@ -7,6 +7,13 @@ import { limitation } from '../../data/limitation';
 import { capabilities } from '../../data/capabilities';
 
 const Home = () => {
+    const [items, setItems] = useState([]);
+
+    const clickHandler: any = (val: any) => {
+
+        localStorage.setItem('items', JSON.stringify(val));
+    }
+
     return (
         <>
             <div className={styles.main_container}>
@@ -15,7 +22,9 @@ const Home = () => {
                         <span className={styles.header_span}>Examples</span>
                         {examples.map((data: IReasearchGate) => {
                             return (
-                                <Link to="/chat">{data.title}</Link>
+                                <Link onClick={() => clickHandler(data.title)} to='/chat'>{data.title}</Link>
+
+
                             )
                         })}</section>
 
